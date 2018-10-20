f6f76c548dc649a42be3eb1c825638540b32353c
Saransh Mittal <saranshmanu@yahoo.co.in>
2018-10-20 10:00:22 +0530

    Completed learning use of turicreate by apple and basic training

--- Training using Turi/playground.py
+++ Training using Turi/playground.py
@@ -1,6 +1,14 @@
 import turicreate as turi
 url = "dataset/"
 data = turi.image_analysis.load_images(url)
-data["foodType"] = data["path"].apply(lambda path: "Rice" if "rice" in path else "Soup")
+data["foodType"] = data["path"].apply(lambda path: "AirPods" if "AirPods" in path else "MacBook")
 data.save("rice_or_soup.sframe")
 data.explore()
+
+dataBuffer = turi.SFrame("rice_or_soup.sframe")
+trainingBuffers, testingBuffers = dataBuffer.random_split(0.9)
+model = turi.image_classifier.create(trainingBuffers, target="foodType", model="squeezenet_v1.1")
+evaluations = model.evaluate(testingBuffers)
+print evaluations["accuracy"]
+model.save("rice_or_soup.model")
+model.export_coreml("RiceSoupClassifier.mlmodel")
